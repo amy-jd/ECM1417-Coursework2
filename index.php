8162b7db6dddcabae9aadb4a600571b8b145b72f
Amelia Denney <amelia.denney@gmail.com>
2025-03-06 16:08:35 +0000

    Functional index and registration page

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 session_start();
 
 $registered = isset($_SESSION['username']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    setcookie('username', '', time() - 3600, '/');
+    setcookie('avatar', '', time() - 3600, '/');
+}
 ?>
 
 
@@ -14,6 +19,9 @@ $registered = isset($_SESSION['username']);
             <?php if(isset($_COOKIE['username'])) { ?>
                 <h1>Welcome to Pairs</h1>
                 <a href="pairs.php">Click here to play</a>
+                <form method="POST">
+                    <button type="submit" name="unregister">unregister</button>
+                </form>
             <?php } else { ?>
                 <h1>You're not using a registered session? <a href="registration.php">Register now</a></h1>
             <?php } ?>
--- registration.php
+++ registration.php
@@ -5,6 +5,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $_SESSION['username'] = $username;
     setcookie("username", $username, time() + (86400 * 30), "/");
+
+    $avatar = $_POST['avatar'];
+    $_SESSION['avatar'] = $avatar;
+    setcookie("avatar", $avatar, time() + (86400 * 30), "/");
 }
 ?>
 
@@ -17,6 +21,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div id="main">
             <form method="POST" action="">
                 <input type="text" name="username" placeholder="Enter Name">
+                <select name="avatar">
+                    <option value="🤖">🤖 grrr</option>
+                    <option value="🐱">🐱 meow</option>
+                </select>
                 <input type="submit" value="Submit">
             </form>
         </div>
