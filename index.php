18355e25309c1eb6b1cf04a20ef69c2c50a8949d
Amelia Denney <amelia.denney@gmail.com>
2025-03-06 15:06:33 +0000

    Created initial landing page

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Hello, World!";
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <div id="main">
+            <h1>Welcome to Pairs</h1>
+            <button>Click here to play</button>
+            <h1>You're not using a registered session? <a href="http://127.0.0.1:8000/registration.php">Register now</a></h1>
+        </div>
+    </body>
+</html>
